e3fb982fc363ffca2d6c933462537818c7322e6c
Hamid Ibrahim <hamid.ibrahim@ubagroup.com>
2022-07-14 16:12:39 +0100

    sum of two numbers to a target

---
+++ script.js
@@ -0,0 +1,27 @@
+/**
+ * The function "sumToGivenTarget()" takes in two arguements.
+ * The first one is an array of numbers.
+ * The second one is the target.
+ * The function returns an array of two different numbers that can be summed up to the given target. 
+ * A single forloop is used here to achieve O(n) time complexity.
+ * 
+ */
+ function sumToGivenTarget(numsArray, targetNum){
+    let addends  = new Set();
+    let result = [];
+    for(let i=0; i < numsArray.length; i++){
+      let val = numsArray[i];
+      if(addends.has(val)){
+        result.push(targetNum - val);
+        result.push(val);
+        break;
+      } else{
+        addends.add(targetNum-val);
+     }
+    }
+    return result;
+}
+let nums = [1, -2, 1, 0, 5], target =3;
+console.log(sumToGivenTarget(nums, target));
+
+// https://github.com/Hambrospecial/sumtotarget.git
